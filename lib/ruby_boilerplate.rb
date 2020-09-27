fd25b6184d30cb010ac56c20272663e9595cb5b1
Chloe <chloe.wong@madetech.com>
2020-09-27 14:41:39 +0100

    first commit

---
+++ lib/ruby_boilerplate.rb
@@ -0,0 +1,5 @@
+class StatsCalc
+    def stats_calculator(input)
+
+    end
+end
---
+++ spec/ruby_boilerplate_spec.rb
@@ -0,0 +1,15 @@
+require './lib/ruby_boilerplate'
+
+#failing test
+describe StatsCalc do
+    let(:stats_calc) {StatsCalc.new}
+
+    describe "tests for an input of array size 1" do
+        context "given an array with an element of 1" do
+            it "will return an 1 as an integer" do
+                expect(stats_calc.stats_calculator([1])).to eq(1)
+            end
+        end
+    end
+end
+  
